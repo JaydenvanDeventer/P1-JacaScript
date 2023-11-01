3cd55cab0403d3851365cd0f734571e149216fcf
JaydenvanDeventer <9021962@student.zadkine.nl>
2023-11-01 13:26:45 +0100

    opdracht 6.1

---
+++ opdracht 6.1/app.js
@@ -0,0 +1,10 @@
+function kleur_aanpassen(){
+document.getElementById("titles").innerHTML= "hallo";
+}
+
+function kleur_aanpassen(){
+    document.getElementById("titles").style.color= "blue";
+    console.log("inhoud aangepast")
+}
+kleur_aanpassen()
+
