ca1e01877a2fccc64987959c94b442856583b96c
JaydenvanDeventer <9021962@student.zadkine.nl>
2023-11-08 10:05:51 +0100

    update 8.1

--- opdracht 8.1/app.js
+++ opdracht 8.1/app.js
@@ -3,24 +3,28 @@ let answer = document.getElementById("answer");
 function add() {
   let num1 = parseFloat(document.getElementById("num1").value);
   let num2 = parseFloat(document.getElementById("num2").value);
-  if (num1 < 0) {
-    let result = (answer.textContent =
-      "antwoord: " + "Voeg een geldig cijfer in.");
-  }
-  if (num2 < 0) {
-    let result = (answer.textContent =
-      "antwoord: " + "Voeg een geldig cijfer in.");
+  
+  if (num1 > 0 && num2 > 0) {
+    const result = num1 + num2
+    document.getElementById('answer').textContent = `antwoord: ${result}`;
   } else {
-    let result = num1 + num2;
+    document.getElementById('answer').textContent = `het getal is te laag`;
+  }
     console.log(result);
     answer.textContent = "antwoord: " + result;
   }
-}
+
 
 function subtract() {
   let num1 = parseFloat(document.getElementById("num1").value);
   let num2 = parseFloat(document.getElementById("num2").value);
-  let result = num1 - num2;
+  
+  if (num1 > 0 && num2 > 0) {
+    const result = num1 - num2
+    document.getElementById('answer').textContent = `antwoord: ${result}`;
+  } else {
+    document.getElementById('answer').textContent = `het getal is te laag`;
+  }
   console.log(result);
   answer.textContent = "antwoord: " + result;
 }
@@ -28,14 +32,26 @@ function subtract() {
 function multiply() {
   let num1 = parseFloat(document.getElementById("num1").value);
   let num2 = parseFloat(document.getElementById("num2").value);
-  let result = num1 * num2;
+  
+  if (num1 > 0 && num2 > 0) {
+    const result = num1 * num2
+    document.getElementById('answer').textContent = `antwoord: ${result}`;
+  } else {
+    document.getElementById('answer').textContent = `het getal is te laag`;
+  }
   console.log(result);
   answer.textContent = "antwoord: " + result;
 }
 function divide() {
   let num1 = parseFloat(document.getElementById("num1").value);
   let num2 = parseFloat(document.getElementById("num2").value);
-  let result = num1 / num2;
+  
+  if (num1 > 0 && num2 > 0) {
+    const result = num1 / num2
+    document.getElementById('answer').textContent = `antwoord: ${result}`;
+  } else {
+    document.getElementById('answer').textContent = `het getal is te laag`;
+  }
   console.log(result);
   answer.textContent = "antwoord: " + result;
 }
